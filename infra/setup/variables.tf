7c62eef00ff693d706d16a0747c355b9746b0e68
purvender <purvender@gmail.com>
2024-07-08 13:09:09 +0530

    textjmnakjklk

--- infra/setup/variables.tf
+++ infra/setup/variables.tf
@@ -1,6 +1,6 @@
 variable "tf_state_bucket" {
   description = "Name of S3 bucket in AWS for storing TF state"
-  default     = "purvender"
+  default     = "purvenderbucket"
 }
 
 variable "tf_state_lock_table" {
