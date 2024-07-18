a6a50adff91e00cfb85114dbce8e27151b965b65
purvender <purvender@gmail.com>
2024-07-18 10:17:32 +0530

    added ecs service

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -22,3 +22,15 @@ variable "db_password" {
   description = "Password for the Terraform database"
   default     = "recipeapp"
 }
+
+variable "ecr_proxy_image" {
+  description = "Path to the ECR repo with the proxy image"
+}
+
+variable "ecr_app_image" {
+  description = "Path to the ECR repo with the API image"
+}
+
+variable "django_secret_key" {
+  description = "Secret key for Django"
+}
